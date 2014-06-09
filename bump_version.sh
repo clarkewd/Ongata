7588f9a0fc8576524acc7857d39073ec48bea787
NanoTech <nanotech@nanotechcorp.net>
2014-06-09 15:23:04 -0600

    Remove settings bundle
    It only contained version info; not useful enough
    to justify an entry in the user's settings app.

--- bump_version.sh
+++ bump_version.sh
@@ -2,8 +2,6 @@
 
 cd `dirname $0`
 
-SETTINGS_PLIST=`pwd`/Settings.bundle/Root.plist
-
 agvtool bump
 
 MARKETING_VERSION=`agvtool what-marketing-version -terse | grep OpenVBX | sed "s/[^=]*=//"`
@@ -13,7 +11,3 @@ REV=`svnversion -n`
 VERSION_STRING="$MARKETING_VERSION.$BUILD_NUMBER.$REV"
 
 echo "Version: $VERSION_STRING"
-
-# PlistBuddy makes it easy for us to edit nested values w/in plists.  It seems to come
-# standard with Mac OS X, but maybe it comes from the Dev Tools.  Not sure.
-/usr/libexec/PlistBuddy -c "Set PreferenceSpecifiers:1:DefaultValue $VERSION_STRING" Settings.bundle/Root.plist
